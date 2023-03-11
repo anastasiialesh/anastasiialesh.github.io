a005d7b7f6328393711c6af9694d1a629bc0fc94
anastasiialesh <122308292+anastasiialesh@users.noreply.github.com>
2023-03-10 20:38:31 -0800

    added animation

--- art-center/script.js
+++ art-center/script.js
@@ -80,3 +80,21 @@ $('.member-tab h2').click(function() {
   )
   $('.member-content').toggleClass('hide')
 })
+
+let timer=0;
+window.addEventListener('load', (event) => {
+  let intersectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setTimeout(function() { entry.target.classList.add('up'); },timer);
+        timer +=50;
+        intersectionObserver.unobserve(entry.target);
+        setTimeout(function(){ timer = 0;}, 1000)
+      }
+    });
+  });
+
+  document.querySelectorAll('.animate').forEach(obj =>{
+    intersectionObserver.observe(obj);
+    });
+});
